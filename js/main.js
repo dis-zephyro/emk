c7fd1f3d5f5003d83351adbfa788c6634e315d46
dis-zephyro <zephyro@yandex.ru>
2015-03-12 03:06:37 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function(){
+    var mr_input="input[type=text]";
+    $(mr_input).each(function() {    $(this).data('stext',$(this).val());                                               });
+    $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
+    $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
+});
+
+
+
+var slider = $('.slider');
+slider.owlCarousel({
+    loop:true,
+    margin:10,
+    items: 1
+});
+
+$('.slide-next').click(function() {
+    slider.trigger('next.owl.carousel');
+});
+
+$('.slide-prev').click(function() {
+    slider.trigger('prev.owl.carousel');
+});
+
+
+
+var customer = $('.customer-slide');
+customer.owlCarousel({
+    loop:true,
+    margin:10,
+    items: 3
+});
+
+$('.next').click(function() {
+    customer.trigger('next.owl.carousel');
+});
+
+$('.prev').click(function() {
+    customer.trigger('prev.owl.carousel');
+});
+
+

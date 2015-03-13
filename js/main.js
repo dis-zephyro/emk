87bededcf81ed70124a57d8ba380dd7191cd1950
dis-zephyro <zephyro@yandex.ru>
2015-03-14 02:30:08 +0300

    start

--- js/main.js
+++ js/main.js
@@ -24,20 +24,21 @@ $('.slide-prev').click(function() {
 
 
 
-var customer = $('.customer-slide');
-customer.owlCarousel({
-    loop:true,
-    margin:10,
-    items: 3
+$('.customer-slide').jCarouselLite({
+    btnNext: '.customer-next',
+    btnPrev: '.customer-prev',
+    speed: 500,
+    visible: 3
 });
 
-$('.customer-next').click(function() {
-    customer.trigger('next.owl.carousel');
+
+$('.gallery-slide').jCarouselLite({
+    btnNext: '.gallery-next',
+    btnPrev: '.gallery-prev',
+    speed: 500,
+    visible: 5
 });
 
-$('.customer-prev').click(function() {
-    customer.trigger('prev.owl.carousel');
-});
 
 $(".callback, .remind, .enter").fancybox({
     "padding" : 0,
@@ -47,3 +48,56 @@ $(".callback, .remind, .enter").fancybox({
 $('.modal-close, .modal-body button[type="reset"]').click(function(){
     $.fancybox.close();
 });
+
+$(".gallery-item, .modal-link").fancybox({
+    "padding" : 0
+});
+
+// Подключние Яндекс-Карты
+
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [56.8369,60.5827],
+        zoom: 16,
+        controls: ['smallMapDefaultSet']
+    });
+
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Собственный значок метки'
+    }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/placemark.png ',
+        // Размеры метки.
+        iconImageSize: [70, 79],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-50, -130]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.geoObjects.add(myPlacemark);
+}
+
+// End of cart
+
+
+/* Поле отправки файла */
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }
+    var filename = str.slice(i);
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
